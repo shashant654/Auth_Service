7631b32209a982d680830a54a2e2e329f619b6eb
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-07-11 01:32:47 +0530

    add bcrypt password

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./routes/index");
 
 const app = express();
 
 const prepareAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use('/api',apiRoutes)
+
   app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
   });
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const UserController = require('../../controllers/user-controller')
+
+const router = express.Router()
+
+router.post('/signUp', UserController.create)
+
+module.exports = router
